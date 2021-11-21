4e1ba7eb847d7571dd5abdf38e570295082a6f2c
Bruno Coitinho <bruno.coitinho@mppb.mp.br>
2021-11-21 16:27:35 -0300

    Item 36 Completed

--- react-complete-guide/src/components/ExpenseItem.js
+++ react-complete-guide/src/components/ExpenseItem.js
@@ -1,12 +1,17 @@
 import './ExpenseItem.css';
 
 function ExpenseItem() {
+
+  const expenseDate = new Date(2021, 2, 15);
+  const expenseTitle = "Car Insurance";
+  const expenseAmount = 223.54;
+
   return (
     <div className="expense-item">
-      <div>March 15th 2021</div>
+      <div>{expenseDate.toISOString()}</div>
       <div className="expense-item__description">
-          <h2>Car Insurance</h2>
-          <div className="expense-item__price">$223.54</div>
+          <h2>{expenseTitle}</h2>
+          <div className="expense-item__price">${expenseAmount}</div>
       </div>
     </div>
   );
