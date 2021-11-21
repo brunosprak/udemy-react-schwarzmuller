51105a71a07bc394d5fe09752c120ed1e39afbf1
Bruno Coitinho <bruno.coitinho@mppb.mp.br>
2021-11-21 16:17:21 -0300

    Item 34 Completed

--- react-complete-guide/src/components/ExpenseItem.js
+++ react-complete-guide/src/components/ExpenseItem.js
@@ -1,5 +1,13 @@
 function ExpenseItem() {
-    return <h2>Expense item!</h2>
+  return (
+    <div>
+      <div>March 15th 2021</div>
+      <div>
+          <h2>Car Insurance</h2>
+          <div>$223.54</div>
+      </div>
+    </div>
+  );
 }
 
 export default ExpenseItem;
