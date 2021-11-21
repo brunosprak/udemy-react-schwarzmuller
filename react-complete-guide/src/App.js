84786151c4ba482b848f42d6d85d0220758f5829
Bruno Coitinho <bruno.coitinho@mppb.mp.br>
2021-11-21 16:08:58 -0300

    Item 33 Completed

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,7 +1,10 @@
+import ExpenseItem from './components/ExpenseItem';
+
 function App() {
   return (
     <div>
       <h2>Let's get started!</h2>
+      <ExpenseItem>This is also visible.</ExpenseItem>
     </div>
   );
 }
