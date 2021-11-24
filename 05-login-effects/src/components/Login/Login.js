4a306b2cc31de7a694298541ad33624fdd95d7a2
Bruno Coitinho <bruno.coitinho@mppb.mp.br>
2021-11-24 08:36:46 -0300

    125. Building & Using a Custom Context Provider Component Completed

--- 05-login-effects/src/components/Login/Login.js
+++ 05-login-effects/src/components/Login/Login.js
@@ -1,10 +1,15 @@
-import React, { useState, useEffect,  useReducer } from 'react';
+import React, { useState, useEffect,  useReducer, useContext } from 'react';
 
 import Card from '../UI/Card/Card';
 import classes from './Login.module.css';
 import Button from '../UI/Button/Button';
+import AuthContext from "../../store/auth-context";
+
 
 const emailReducer = (state, action) => {
+
+
+
   if(action.type === 'USER_INPUT'){
     return {value:action.val, isValid: action.val.includes("@")};
   }
@@ -27,6 +32,10 @@ const passwordReducer = (state, action) => {
 }
 
 const Login = (props) => {
+
+  
+  const authCtx = useContext(AuthContext);
+
   // const [enteredEmail, setEnteredEmail] = useState('');
   // const [emailIsValid, setEmailIsValid] = useState();
   // const [enteredPassword, setEnteredPassword] = useState('');
@@ -83,7 +92,7 @@ const Login = (props) => {
 
   const submitHandler = (event) => {
     event.preventDefault();
-    props.onLogin(emailState.value, passwordState.value);
+    authCtx.onLogin(emailState.value, passwordState.value);
   };
 
   return (
--- 05-login-effects/src/store/auth-context.js
+++ 05-login-effects/src/store/auth-context.js
@@ -1,8 +1,44 @@
-import React from 'react';
-
+import React, { useState, useEffect } from "react";
 
 const AuthContext = React.createContext({
-    isLoggedIn: false
+  isLoggedIn: false,
+  onLogout: (email, password) => {},
 });
+
+export const AuthContextProvider = (props) => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  useEffect(() => {
+    const alreadyLoggedIn = localStorage.getItem("isLoggedIn");
+
+    if (alreadyLoggedIn === "1") {
+      setIsLoggedIn(true);
+    }
+  }, []);
+
+  const loginHandler = (email, password) => {
+    // We should of course check email and password
+    // But it's just a dummy/ demo anyways
+    localStorage.setItem("isLoggedIn", "1");
+    setIsLoggedIn(true);
+  };
+
+  const logoutHandler = () => {
+    setIsLoggedIn(false);
+    localStorage.removeItem("isLoggedIn");
+  };
+
+  return (
+    <AuthContext.Provider
+      value={{
+        isLoggedIn: isLoggedIn,
+        onLogout: logoutHandler,
+        onLogin: loginHandler
+      }}
+    >
+      {props.children}
+    </AuthContext.Provider>
+  );
+};
 
 export default AuthContext;
