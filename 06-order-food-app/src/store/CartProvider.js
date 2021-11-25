8d9bb4a36711fdcd07e258ebe2c985db70806c7c
Bruno Coitinho <bruno.coitinho@mppb.mp.br>
2021-11-25 08:24:30 -0300

    144. Adding a Cart Reducer Completed

--- 06-order-food-app/src/store/CartProvider.js
+++ 06-order-food-app/src/store/CartProvider.js
@@ -1,16 +1,37 @@
+import { useReducer } from "react";
 import CartContext from "./cart-context";
+
+const defaultCartState = {
+    items: [],
+    totalAmount: 0
+};
+
+const cartReducer = (state, action) => {
+
+    if(action === 'ADD'){
+        const updatedItems = state.items.concat(action.item);
+        const updatedTotalAmount = state.totalAmount + action.item.price * action.item.amount;
+        return {items: updatedItems, totalAmount: updatedTotalAmount};
+    } else if (action === 'REMOVE'){
+
+    }
+    return defaultCartState;
+};
 
 const CartProvider = props => {
 
+    const [cartState, dispatchCartAction]  = useReducer(cartReducer, defaultCartState);
+
     const addItemHandler = (item) => {
+        dispatchCartAction({ type: 'ADD', item: item});
 
     }
     const removeItemHandler = (id) => {
-        
+        dispatchCartAction({ type: 'REMOVE', id: id});
     }
     const cartContext = {
-        items: [],
-        totalAmount: 0,
+        items: cartState.items,
+        totalAmount: cartState.totalAmount,
         addItem: addItemHandler,
         removeItem: removeItemHandler,
       };
