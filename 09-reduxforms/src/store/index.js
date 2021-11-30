3cb5bfd34fc03c8ee26d99218a593327b67bb4b3
Bruno Coitinho <bruno.coitinho@mppb.mp.br>
2021-11-30 16:07:37 -0300

    234. Using Redux Data in React Components Completed

--- 09-reduxforms/src/store/index.js
+++ 09-reduxforms/src/store/index.js
@@ -1,10 +1,10 @@
 import { createStore } from 'redux';
 
 const counterReducer = (state = {counter: 0}, action) => {
-    if(action.type = 'INC'){
+    if(action.type === 'INC'){
         return {counter: state.counter + 1};
     }
-    if(action.type = 'DEC'){
+    if(action.type === 'DEC'){
         return {counter: state.counter - 1};
     }
     return state;
