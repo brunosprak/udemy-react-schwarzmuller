abfc9fc9b7bee565b8a504321c7e74191a4e1d38
Bruno Coitinho <bruno.coitinho@mppb.mp.br>
2021-11-30 16:11:41 -0300

    235. Incomplete

--- 09-reduxforms/src/components/Counter.js
+++ 09-reduxforms/src/components/Counter.js
@@ -1,9 +1,10 @@
 import classes from './Counter.module.css';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 
 const Counter = () => {
 
   const counter = useSelector(state => state.counter);
+  
 
   const toggleCounterHandler = () => {};
 
@@ -11,6 +12,10 @@ const Counter = () => {
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
       <div className={classes.value}>{counter}</div>
+      <div>
+        <button>Increment</button>
+        <button>Decrement</button>
+      </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
