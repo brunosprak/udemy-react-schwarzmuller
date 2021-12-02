541b6a5a0013b3940b3a3708365e4e324e7ca9aa
Bruno Coitinho <bruno.coitinho@mppb.mp.br>
2021-12-02 06:47:24 -0300

    273. Using Switch and exact For Configuring RoutesCompleted

--- 10-routers/src/App.js
+++ 10-routers/src/App.js
@@ -3,21 +3,24 @@ import Header from './components/Header';
 import ProductDetail from './pages/ProductDetail';
 import Products from './pages/Products';
 import Welcome from './pages/Welcome';
+import { Switch } from 'react-router';
 
 function App() {
   return (
     <div>
       <Header />
       <main>
-        <Route path='/welcome'>
-          <Welcome />
-        </Route>
-        <Route path='/products'>
-          <Products />
-        </Route>
-        <Route path='/product-detail/:productId'>
-          <ProductDetail />
-        </Route>
+        <Switch>
+          <Route path='/welcome'>
+            <Welcome />
+          </Route>
+          <Route path='/products' exact>
+            <Products />
+          </Route>
+          <Route path='/products/:productId'>
+            <ProductDetail />
+          </Route>
+        </Switch>
       </main>
     </div>
   );
--- 10-routers/src/pages/Products.js
+++ 10-routers/src/pages/Products.js
@@ -1,11 +1,13 @@
+import { Link } from "react-router-dom";
+
 const Products = () => {
   return (
     <section>
       <h1>The products page</h1>
       <ul>
-        <li>Product 1</li>
-        <li>Product 2</li>
-        <li>Product 3</li>
+        <li><Link to="/products/1">Product 1</Link></li>
+        <li><Link to="/products/2">Product 2</Link></li>
+        <li><Link to="/products/3">Product 3</Link></li>
       </ul>
     </section>
   );
